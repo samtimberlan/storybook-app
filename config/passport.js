040638db52baed2015cb0f3e7db6916c936f4f69
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2018-01-15 21:43:03 +0100

    Created DB schema

--- config/passport.js
+++ config/passport.js
@@ -13,8 +13,17 @@ module.exports = () => {
         proxy: true
       },
       (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log(profile);
+        // console.log(accessToken);
+        // console.log(profile);
+        const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+
+        const newUser = {
+          googleID : profile.id,
+          firstName : profile.name.givenName,
+          lastName : profile.name.familyName,
+          email : profile.emails[0].value,
+          image : image
+        }
       }
     )
   );
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const keys = require('./config/keys');
+
+//Load model
+require('./models/user');
 
 //Load passport
 require('./config/passport')('passport');
 
 //Load Routes
 const auth = require('./routes/auth');
+
+//Map global promises
+mongoose.Promise = global.Promise;
+
+//Mongoose Connect
+mongoose.connect(keys.mongoURI, {useMongoClient: true})
+.then(()=>{console.log('MongoDB connected')})
+.catch((err) => console.log(err));
 
 const app = express();
 
