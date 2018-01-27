430972d679c9233e2e198057b861324d27bc79fc
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2018-01-27 14:09:34 +0100

    Edit Access control

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -18,5 +18,20 @@ module.exports = {
   },
   select: function(selected, options){
     return options.fn(this).replace( new RegExp(' value=\"' + selected + '\"'), '$& selected="selected"').replace( new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
+  }, 
+  editIcon : function(storyUser, loggedUser, storyId, floating = true){
+    if(storyUser === loggedUser){
+      if(floating){
+        return `<a href="/stories/edit/{{id}}" class="btn-floating halfway-fab teal"><i class="fa fa-pencil"></i>
+        </a>`
+      }
+      else{
+        return `<a href="/stories/edit/{{id}}"><i class="fa fa-pencil"></i>
+        </a>`
+      }
+    }
+    else{
+      return '';
+    }
   }
 }
--- routes/stories.js
+++ routes/stories.js
@@ -12,6 +12,7 @@ router.get('/', (req, res)=>{
    //Important:Filter story by status
    Story.find({status : 'public'})
    .populate('user')
+   .sort({date : 'desc'})
    .then(stories =>{
      res.render('stories/index', {stories : stories});
    });
@@ -70,9 +71,13 @@ router.get('/edit/:id', ensureAuthenticated, (req, res)=>{
     _id : req.params.id
   })
   .then(story => {
+    if(story.user !== req.user.id){
+      res.redirect('/stories');
+    }else{
     res.render('stories/edit', {
       story : story
     });
+  }
   });
 });
 
