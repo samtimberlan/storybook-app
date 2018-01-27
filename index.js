3bd9f93842d6abf9ff1ff12cb66998a867c1af6c
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2018-01-27 21:56:44 +0100

    Add keys

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ mongoose.Promise = global.Promise;
 
 //Mongoose Connect
 //mongodb://localhost/storybook
-mongoose.connect('mongodb://localhost/storybook', {useMongoClient: true})
+mongoose.connect(keys.mongoURI, {useMongoClient: true})
 .then(()=>{console.log('MongoDB connected')})
 .catch((err) => console.log(err));
 
